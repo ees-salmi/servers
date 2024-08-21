5a97b1d9eb4054d2f62f8ef464d76db7946a6b55
mustapha <elmustaphaes.salmi@gmail.com>
2024-08-21 23:51:24 +0200

    add verify otp

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ const verifySid = process.env.TWILIO_VERIFY_SID;
 const client = twilio(accountSid, authToken);
 
 // Endpoint to send OTP
-app.post('/api/send-otp', (req, res) => {
+app.get('/api/send-otp', (req, res) => {
   const { phoneNumber } = req.body;
   console.log(phoneNumber);
   client.verify.v2.services(verifySid)
-    .verifications.create({ to: "+212606060481", channel: 'sms' })
+    .verifications.create({ to: phoneNumber, channel: 'sms' })
     .then(verification => {
       res.status(200).json({ status: verification.status });
     })
     .catch(error => {
       res.status(500).json({ error: error.message });
     });
-    res.send('api phone!');
 });
 
 app.listen(port, () => {
@@ -33,9 +32,26 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!!');
 });
 
 app.get('/test', (req, res) => {
   res.send('test World!');
+});
+app.get('/verify-otp', (req, res) => {
+  const phoneNumber = req.query.phone;
+  const otpCode = req.query.code;
+
+  client.verify.v2.services(process.env.TWILIO_VERIFY_SID)
+      .verificationChecks.create({ to: phoneNumber, code: otpCode })
+      .then(verification_check => {
+          if (verification_check.status === 'approved') {
+              res.status(200).send('OTP verified successfully');
+          } else {
+              res.status(400).send('OTP verification failed');
+          }
+      })
+      .catch(error => {
+          res.status(500).send('Error verifying OTP');
+      });
 });
